0d46ccc35dc43af01c2db00ebf68c19e2eca6cda
choijw1004 <97choijw@gmail.com>
2025-02-24 16:23:23 +0900

    [Init] Spring Project Init

---
+++ BackEnd_Spring/TimeStock/src/test/java/com/example/TimeStock/TimeStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TimeStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
