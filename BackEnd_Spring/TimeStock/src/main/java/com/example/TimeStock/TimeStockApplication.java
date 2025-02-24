b8de0df60cb1a1eed472da4951f246c18606b419
choijw1004 <97choijw@gmail.com>
2025-02-24 16:23:59 +0900

    [feat] KIS 1종목 주가 데이터 API 완료

---
+++ BackEnd_Spring/TimeStock/src/main/java/com/example/TimeStock/TimeStockApplication.java
@@ -0,0 +1,13 @@
+package com.example.TimeStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeStockApplication.class, args);
+	}
+
+}
